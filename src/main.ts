7ed78f066064ff890b310b42f57d1a1af611d745
TimiSz <timi.taylor168@gmail.com>
2017-11-17 18:41:14 +0100

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { AppRoutingModule } from './app/app-routing.module';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic().bootstrapModule(AppModule);
