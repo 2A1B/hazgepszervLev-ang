04229d7fc37745a7ed92a3550004d94d1ab0005c
nyirfacska <apro.ancsa@gmail.com>
2017-11-28 06:22:46 +0100

    JobReport model class

---
+++ src/app/_models/JobReport.ts
@@ -0,0 +1,10 @@
+export class JobReport {
+  customer_name: string;
+  customer_address: string;
+  customer_phone: string;
+  planned_delivery: string;
+  problem_review: string;
+  job_title: string;
+  status: string;
+  uuid: string;
+}
