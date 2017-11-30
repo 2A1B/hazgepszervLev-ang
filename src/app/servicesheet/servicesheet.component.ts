64db0ef425a12479f1ffb1e614dc89b523e52872
TimiSz <timi.taylor168@gmail.com>
2017-11-30 23:47:01 +0100

    2A1B-42-SERVICE_SHEET
    Remove unused variable.

--- src/app/servicesheet/servicesheet.component.ts
+++ src/app/servicesheet/servicesheet.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {AngularFireDatabase} from 'angularfire2/database';
 import {Observable} from 'rxjs/Observable';
 import {ServiceSheet} from '../_models/ServiceSheet';
-import {JobReport} from '../_models/JobReport';
 
 @Component({
   selector: 'app-servicesheet',
@@ -11,11 +10,8 @@ import {JobReport} from '../_models/JobReport';
 })
 export class ServicesheetComponent implements OnInit {
   reports: Observable<any[]>;
-  openedReports: Observable<any[]>;
   
   constructor(public db: AngularFireDatabase) {
-  this.openedReports = db.list<JobReport>('jobreports', ref => ref.orderByChild('status').equalTo('open')).valueChanges();
-   
     this.reports = db.list<ServiceSheet>('servicesheets', ref => ref.orderByChild('customer_name')).valueChanges();
   }
   ngOnInit() {
