c90b6e67bb45776803e2987e9a5b1d2c1bb1d203
nyirfacska <apro.ancsa@gmail.com>
2017-12-10 15:55:17 +0100

    2A1B-40-CALENDAR

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import { HomeComponent } from './home/home.component';
 import { JobreportsComponent } from './jobreports/jobreports.component';
 import { ServicesheetComponent } from './servicesheet/servicesheet.component';
 import { ServiceSheetPdfComponent } from './service-sheet-pdf/service-sheet-pdf.component';
+import {CalComponent} from './calendar/calendar.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent, children: [
     { path: 'jobreports', component: JobreportsComponent, outlet: 'side' },
-    { path: 'servicesheet', component: ServicesheetComponent, outlet: 'side' }
+    { path: 'servicesheet', component: ServicesheetComponent, outlet: 'side' },
+    { path: 'calendar', component: CalComponent, outlet: 'side' }
     ]
   },
   { path: 'service/pdf/:id', component: ServiceSheetPdfComponent }
