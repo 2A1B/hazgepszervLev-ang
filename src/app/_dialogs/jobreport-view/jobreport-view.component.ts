8dcb80e7c7eebc73a89b44666e1422eb62856ff6
TimiSz <timi.taylor168@gmail.com>
2017-12-14 07:32:24 +0100

    Eliminate unnecessary filebase call.

--- src/app/_dialogs/jobreport-view/jobreport-view.component.ts
+++ src/app/_dialogs/jobreport-view/jobreport-view.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import {AngularFireDatabase, AngularFireList} from 'angularfire2/database';
 import {MatDialog, MatDialogConfig, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 import {ServiceSheet} from "../../_models/ServiceSheet";
 import {Observable} from 'rxjs/Observable';
@@ -12,13 +11,10 @@ import { JobReport } from '../../_models/JobReport';
 })
 export class JobreportViewComponent implements OnInit {
 
-  itemsRef: AngularFireList<any>;
   today: string;
   jobreport: JobReport;
   constructor(@Inject(MAT_DIALOG_DATA) public data: any,
-              public dialog: MatDialogRef<JobreportViewComponent>,
-              public db: AngularFireDatabase) {
-    this.itemsRef = db.list('servicesheets');
+              public dialog: MatDialogRef<JobreportViewComponent>) {
     this.jobreport = data.item;
     const ddd = this.jobreport.customer_name;
   }
